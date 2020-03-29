12fd039277af10d0b4fd7ab2e489ad068c53d70d
Benjamin Ludwig <atlassian@benjamin-ludwig.de>
2020-03-29 09:05:53 +0200

    black status bar/ todos

---
+++ JahPress/JahPress/Extensions/UIViewController+StatusBar.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController+StatusBar.swift
+//  JahPress
+//
+//  Created by Benjamin Ludwig on 29.03.20.
+//  Copyright © 2020 Benjamin Ludwig. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+extension UIViewController {
+
+    var preferredStatusBarStyle: UIStatusBarStyle {
+        return .darkContent
+    }
+}
